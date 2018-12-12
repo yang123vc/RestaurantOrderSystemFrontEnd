0e0110a4f615ea2d1ca7f4cd9748538eca7cf6e0
Kathleen Law <klaw@worcester.edu>
2018-12-12 08:13:40 -0500

    the moment we switched to entirely front end

--- OrderSystem/src/app/app.component.ts
+++ OrderSystem/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Product } from './Product';
+
 
 @Component({
   selector: 'app-root',
@@ -7,5 +11,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Restaurant Extravaganza';
+  product: Product = new Product();
   whichFood = 11;
+  productIdReturn: number = null;
+  constructor(private http: HttpClient) {}
+
+  addNewProduct(price: number, description: string): void {
+      this.http.post<number>('http://localhost:8080/products/new',
+      {price: price, description: description})
+        .subscribe(data => {
+          this.productIdReturn = data;
+        }
+      );
+    }
 }
